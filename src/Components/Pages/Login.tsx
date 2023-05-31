d7a872bea276ca2765bd2138fa8da23da936ad91
Victor-udenna <victorudenna@gmail.com>
2023-05-31 16:55:07 +0100

    edited InputField

--- src/Components/Pages/Login.tsx
+++ src/Components/Pages/Login.tsx
@@ -1,6 +1,9 @@
-
+import { Link } from "react-router-dom";
+import './styles.scss'
 export default function Login(){
     return <>
-    dfrtg
+    
+
+    <p className="login_text">Don't have an accout ? <span className="login_link"><Link to="/Signup">Create an accout</Link></span></p>
     </>
 }
